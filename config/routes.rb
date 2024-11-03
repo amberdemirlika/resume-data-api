e6cc901071d7790fe83cc50258b200b1380e540b
ashoordoushow <ashoor12323@gmail.com>
2024-11-03 12:39:00 -0600

    Add index action for skills and experiences

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  get "/experiences" => "experiences#index"
+  get "/skills" => "skills#index"
+
   # Defines the root path route ("/")
   # root "posts#index"
 end
