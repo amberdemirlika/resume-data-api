46fbf8d580da3bb06d93513659ce69b1d3b38680
jpashley1 <jpashley1@gmail.com>
2024-11-03 14:00:03 -0500

    added Edcuation model with CRUD and Seeds file for data

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,12 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
+  get "/educations" => "educations#index"
+  get "/educations/:id" => "educations#show"
+  post "/educations" => "educations#create"
+  patch "/educations/:id" => "educations#update"
+  delete "/educations/:id" => "educations#destroy"
+  
   # Defines the root path route ("/")
   # root "posts#index"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,27 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'csv'
+
+file = Rails.root.join("db", "education-data.csv")
+
+# Check if the file exists before proceeding
+unless File.exist?(file)
+  puts "File not found: #{file}"
+  exit
+end
+
+CSV.foreach(file, headers: false) do |row|
+  student_id = row[0]
+  start_date = row[1]
+  end_date = row[2]
+  degree = row[3]
+  university = row[4]
+  details = row[5]
+
+  Education.create!(
+    student_id: student_id,
+    start_date: start_date,
+    end_date: end_date,
+    degree: degree,
+    university: university,
+    details: details
+  )
+end
